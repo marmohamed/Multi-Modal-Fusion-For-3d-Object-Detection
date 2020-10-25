289117489725de5ffbc3babc8f0fe5764edf3763
Mariam <maryammohamed61@gmail.com>
2020-10-25 15:58:03 +0200

    add old model

--- src/models/ResnetLidarBEV.py
+++ src/models/ResnetLidarBEV.py
@@ -42,11 +42,8 @@ class ResNetLidarBEV(ResNet):
                 x = resblock(x, channels=192, is_training=is_training, downsample=downsample_arg, scope='resblock3_' + str(i))
             # x = dropout(x, 0.1, 'res_block_3_dropout_' + str(i), training=is_training)
 
-            x = upsample(x, is_training=is_training, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=False, filters=256, kernel_size=4)
-            x = conv(x, 256, kernel=1, stride=1, scope='conv2_upsample' + str(i))
-            x = batch_norm(x, is_training=is_training, scope='bn2_upsample' + str(i))
-            x = relu(x)
-
+            x = upsample(x, is_training=is_training, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=True, filters=256, kernel_size=4)
+           
             res_groups[-1] = conv(res_groups[-1], 256, kernel=1, stride=1, scope='conv2_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1], is_training=is_training, scope='bn2_' + str(i))
             res_groups[-1] = relu(res_groups[-1])
@@ -63,10 +60,7 @@ class ResNetLidarBEV(ResNet):
                 x = resblock(x, channels=256, is_training=is_training, downsample=downsample_arg, scope='resblock4_' + str(i))
             # x = dropout(x, 0.1, 'res_block_4_dropout_' + str(i), training=is_training)
 
-            x = upsample(x, is_training=is_training, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=False, filters=256, kernel_size=4)
-            x = conv(x, 256, kernel=1, stride=1, scope='conv3_upsample' + str(i))
-            x = batch_norm(x, is_training=is_training, scope='bn3_upsample' + str(i))
-            x = relu(x)
+            x = upsample(x, is_training=is_training, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=True, filters=256, kernel_size=4)
 
             res_groups[-1] = conv(res_groups[-1], 256, kernel=1, stride=1, scope='conv3_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1], is_training=is_training, scope='bn3_' + str(i))
