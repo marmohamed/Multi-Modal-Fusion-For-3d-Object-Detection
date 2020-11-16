5ea7b763be1ef583149acff12caa631ba4962a5b
Mariam <maryammohamed61@gmail.com>
2020-11-16 02:03:53 +0200

    change lr - end

--- src/training/detection_trainer.py
+++ src/training/detection_trainer.py
@@ -577,7 +577,7 @@ class EndToEndDetectionTrainer(DetectionTrainer):
         self.branch_params = {
             'opt': self.model.train_op,
             'train_fusion_rgb': True,
-            'lr': 5e-4,
+            'lr': 1e-5,
             'step_size': 2 * 3682,
             'learning_rate': 1e-6,
             'max_lr': 1e-4,
